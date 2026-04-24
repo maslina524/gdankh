c2b1b7a1de126f71a9393c60a2e9d77591d8c172
maslina524 <luk.kirill2804@gmail.com>
2026-04-24 17:59:59 +0300

    added classes

---
+++ src/gdankh/classes.py
@@ -0,0 +1,73 @@
+from enumerations import PlayerColor
+from hsv import Hsv
+from utils import chunks
+
+class ColorChannel:
+    def __init__(self):
+        from_r: int
+        from_g: int
+        from_b: int
+        p_color: PlayerColor
+        blending: bool
+        color_i: int
+        from_opacity: float
+        toggle_opacity: bool
+        inherited_color_i: int
+        hsv: Hsv
+        to_r: int
+        to_g: int
+        to_b: int
+        delta_time: float
+        to_opacity: float
+        duration: float
+        copy_opacity: bool
+
+    @classmethod
+    def from_string(cls, string: str):
+        instance = cls()
+
+        color_chunks = chunks(string.split("_"), 2)
+        print(color_chunks)
+        if len(color_chunks) < 3:
+            return instance
+            
+        for chunk in color_chunks:
+            # https://boomlings.dev/resources/client/level-components/color-string#client-color-string-resource
+            v = chunk[1]
+            match int(chunk[0]): # key
+                case 1:
+                    instance.from_r = int(v)
+                case 2:
+                    instance.from_g = int(v)
+                case 3:
+                    instance.from_b = int(v)
+                case 4:
+                    instance.p_color = PlayerColor(int(v))
+                case 5:
+                    instance.blending = (v == '1')
+                case 6:
+                    instance.color_i = int(v)
+                case 7:
+                    instance.from_opacity = float(v)
+                case 8:
+                    instance.toggle_opacity = bool(int(v))
+                case 9:
+                    instance.inherited_color_i = int(v)
+                case 10:
+                    instance.hsv = Hsv.from_string(v)
+                case 11:
+                    instance.to_r = int(v)
+                case 12:
+                    instance.to_g = int(v)
+                case 13:
+                    instance.to_b = int(v)
+                case 14:
+                    instance.delta_time = float(v)
+                case 15:
+                    instance.to_opacity = float(v)
+                case 16:
+                    instance.duration = float(v)
+                case 17:
+                    instance.copy_opacity = bool(int(v))
+
+        return instance
